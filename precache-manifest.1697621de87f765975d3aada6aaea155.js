303a2a1cb013a6a38409657b5d7bdeb7cbabc944
Phynnex <angalafynetonye@gmail.com>
2020-08-07 13:44:44 +0100

    Updates

---
+++ precache-manifest.1697621de87f765975d3aada6aaea155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0382d6ce4639da0916f9dcb50d874eb8",
+    "url": "/joke_app/index.html"
+  },
+  {
+    "revision": "825b5e10a544490e2605",
+    "url": "/joke_app/static/css/main.e88593d1.chunk.css"
+  },
+  {
+    "revision": "376bdc663f8b68aff3da",
+    "url": "/joke_app/static/js/2.6608ea2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/joke_app/static/js/2.6608ea2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825b5e10a544490e2605",
+    "url": "/joke_app/static/js/main.a4535dfd.chunk.js"
+  },
+  {
+    "revision": "7ba87c6c0abb3970bd49",
+    "url": "/joke_app/static/js/runtime-main.d18de2ca.js"
+  }
+]);
